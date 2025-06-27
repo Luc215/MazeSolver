1e4c8a0b28f44f302da47f542e12557437276943
Lucas Lyubenko <lucaslyubenko9@gmail.com>
2025-06-26 23:35:12 -0500

    Added some stuff for the future.

--- MazeGenerator.cpp
+++ MazeGenerator.cpp
@@ -7,14 +7,73 @@
 
 //might move later to different file idk
 std::vector<std::vector<float>> cells = {};
-// {top,bottom,left, right}
-std::vector<std::vector<int>> borders = {};
+
+// {top,left,right,bottom}
+std::vector<std::vector<bool>> borders = {};
+
+//here we will truly populate cells with walls. true equals wall should be present. else not
+void wallPopulation(int rows, int cols){
+    int totalCells = rows * cols;
+    borders.resize(totalCells);
+    for(int index = 0; index < totalCells; index++){
+        borders[index].resize(4,true);
+    }
+
+
+
+}
+//when calling kruskals to remove walls
+void removeWallBorders(int cell1,int cell2, int cols){
+    //row of cell 1
+    int r1 = cell1 / cols; 
+    //column of cell 1
+    int c1 = cell1 % cols;
+    //row of cell 2
+    int r2 = cell2 / cols; 
+    //column of cell 2
+    int c2 = cell2 % cols;
+
+    //check if next to eachother (same row)
+    if(r1 == r2){
+        //cell1 on the right, cell2 on the left
+        if(c1 > c2){
+            borders[cell1][1] = false;
+            borders[cell2][2] = false;
+
+        }
+        else{
+            borders[cell1][2] = false;
+            borders[cell2][1] = false;
+
+
+        }
+
+    }
+    //looking horizontally
+
+    else{
+        //cell 1 below, cell 2 above
+        if(r1 >r2){
+            borders[cell1][0] = false;
+            borders[cell2][3] = false;
+
+        }
+        else{
+            borders[cell1][3] = false;
+            borders[cell2][0] = false;
+
+        }
+
+    }
+
+
+}
 void grid(sf::RenderWindow& window){
     //row and cols const for now. will change when ui introduced. defines how many cells.
     //why did i decide to use c++? do i hate myself?
-    //would we need to make sure only one wall is printed between cells? yes.
-    const int rows = 10;
-    const int cols = 10;
+    //ok so algo wil go through and break two walls at a time thats what im thinking.
+    const int rows = 50;
+    const int cols = 50;
     const float cellLength = 800/rows;
     const float cellWidth  = 800/cols;
     
@@ -49,11 +108,6 @@ void grid(sf::RenderWindow& window){
 
 }
 
-
-//need to redraw each state
-void redraw_grid(){
-
-}
 //for when we will implement button to make maze generate or not type shit
 void button(){
 
